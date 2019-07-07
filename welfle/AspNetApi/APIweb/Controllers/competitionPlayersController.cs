ff04389b54647609e2b46fa3842f3c3675340f1c
Mateusz Sudak <mateusz.sudak@wp.pl>
2019-07-07 22:18:59 +0200

    Add controller competitionPlayersController

---
+++ welfle/AspNetApi/APIweb/Controllers/competitionPlayersController.cs
@@ -0,0 +1,42 @@
+﻿using RaceTimingDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIweb.Controllers
+{
+    public class competitionPlayersController : ApiController
+    {
+        private RaceTimingEntities db = new RaceTimingEntities();
+        // GET: api/competitionPlayers
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/competitionPlayers/5
+        public IEnumerable<player> Get(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            return db.players.SqlQuery("select * from player p where p.id in (select players_id from competition_players where competition_id=@p0)", id).ToList<player>();
+        }
+
+        // POST: api/competitionPlayers
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/competitionPlayers/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/competitionPlayers/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
